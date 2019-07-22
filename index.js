885218844107604112e1c6eef41fb0e131358133
woodylucas <github email address>
2019-07-22 16:39:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+const findMatch = (drivers, string) => {
+  const sameName = drivers.filter(driver => {
+    if (driver.name === string) {
+      return driver.name;
+    }
+  })
+  return sameName;
+}
